5f7e9fa879526ba30a19d7f6c2aa16a12722d2cf
ljh <2439827731@qq.com>
2024-02-12 21:40:31 +0800

    更新获取操作记录逻辑，删除判空

--- lib/handler/req_missing_data_handler.dart
+++ lib/handler/req_missing_data_handler.dart
@@ -10,23 +10,55 @@ class ReqMissingDataHandler {
         .getSyncRecord(App.userId, reqDev.guid)
         .then((lst) {
       var future = Future.value();
+      var rmList = <OperationRecord>[];
       for (var item in lst) {
         Future t;
         var id = item.data;
         switch (item.module) {
           case Module.device:
             t = DBUtil.inst.deviceDao.getById(id, App.userId).then((v) {
-              item.data = v.toString();
+              if (v == null) {
+                if (item.method != OpMethod.delete) {
+                  rmList.add(item);
+                }
+              } else {
+                item.data = v.toString();
+              }
             });
             break;
           case Module.tag:
             t = DBUtil.inst.historyTagDao.getById(int.parse(id)).then((v) {
-              item.data = v.toString();
+              if (v == null) {
+                if (item.method != OpMethod.delete) {
+                  rmList.add(item);
+                }
+              } else {
+                item.data = v.toString();
+              }
             });
             break;
           case Module.history:
             t = DBUtil.inst.historyDao.getById(int.parse(id)).then((v) {
-              item.data = v.toString();
+              if (v == null) {
+                if (item.method != OpMethod.delete) {
+                  rmList.add(item);
+                }
+              } else {
+                item.data = v.toString();
+              }
+            });
+            break;
+          case Module.historyTop:
+            t = DBUtil.inst.historyDao.getById(int.parse(id)).then((v) {
+              if (v == null) {
+                if (item.method != OpMethod.delete) {
+                  rmList.add(item);
+                }
+              } else {
+                //更新置顶状态，将内容设为空，提高传输效率
+                v.content = "";
+                item.data = v.toString();
+              }
             });
             break;
           default:
@@ -35,7 +67,11 @@ class ReqMissingDataHandler {
         }
         future = future.then((value) => t);
       }
-      return future.then((v) => lst);
+      return future.then((v) {
+        //删除需要移除的item
+        lst.removeWhere((element) => rmList.contains(element));
+        return lst;
+      });
     });
   }
 
