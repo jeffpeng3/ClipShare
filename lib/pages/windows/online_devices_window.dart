35bd7c7979934168513197a586145d24d02051a0
ljh <2439827731@qq.com>
2024-05-23 22:59:38 +0800

    1.[feature] 在线设备弹窗失去焦点后关闭

--- lib/pages/windows/online_devices_window.dart
+++ lib/pages/windows/online_devices_window.dart
@@ -1,5 +1,4 @@
 import 'package:clipshare/main.dart';
-import 'package:clipshare/pages/compact_page.dart';
 import 'package:clipshare/pages/online_devices_page.dart';
 import 'package:desktop_multi_window/desktop_multi_window.dart';
 import 'package:flutter/material.dart';
@@ -20,7 +19,26 @@ class OnlineDevicesWindow extends StatefulWidget {
   }
 }
 
-class _OnlineDevicesWindowState extends State<OnlineDevicesWindow> {
+class _OnlineDevicesWindowState extends State<OnlineDevicesWindow>
+    with WidgetsBindingObserver {
+  @override
+  void initState() {
+    super.initState();
+    //监听生命周期
+    WidgetsBinding.instance.addObserver(this);
+  }
+
+  @override
+  void didChangeAppLifecycleState(AppLifecycleState state) {
+    super.didChangeAppLifecycleState(state);
+    switch (state) {
+      case AppLifecycleState.inactive:
+        widget.windowController.close();
+        break;
+      default:
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
